0c4a9012d16fecd75d868572f43e34507926b6bb
Olga Polishchuk <olgapolichuk@ukr.net>
2023-07-31 18:33:09 +0300

    Update getAllUnits.js

--- controllers/units/getAllUnits.js
+++ controllers/units/getAllUnits.js
@@ -2,7 +2,7 @@ const { Unit } = require("../../models/unit");
 
 const getAllUnits = async (req, res) => {
     
-  const respons = await Unit.find({});
+  const respons = await Unit.find({}).select("-owner");
 
   res.status(200).json(respons);
 };
