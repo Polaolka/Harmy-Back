9345f1f8e2d1c0c75ad00808f4f6384cc2bfe0b4
Olga Polishchuk <olgapolichuk@ukr.net>
2023-07-31 23:16:50 +0300

    Update addDonatsReport.js

--- controllers/donats/addDonatsReport.js
+++ controllers/donats/addDonatsReport.js
@@ -1,8 +1,15 @@
 const { Donat } = require("../../models/donat");
 
+const { RequestError } = require("../../helpers");
+
 const addDonatsReport = async (req, res) => {
   const { _id: userId } = req.user;
   const { id: reqId } = req.params;
+
+  const donatData = await Donat.findOne({ _id: reqId });
+  if (donatData.owner.toString() !== userId.toString()) {
+    throw RequestError(403, "another author");
+  }
 
   const { reportDescr } = req.body;
 
@@ -13,23 +20,37 @@ const addDonatsReport = async (req, res) => {
   const donat = await Donat.findByIdAndUpdate(
     reqId,
     {
-      $set: { isReported: true, isOpen: false, report: {reportPhoto1Url, reportPhoto2Url, reportPhoto3Url, reportDescr} },
+      $set: {
+        isReported: true,
+        isOpen: false,
+        report: {
+          reportPhoto1Url,
+          reportPhoto2Url,
+          reportPhoto3Url,
+          reportDescr,
+        },
+      },
     },
     { new: true }
-  );
+  )
+    .populate("owner", "name")
+    .populate("typeName", "typeName")
+    .populate("unitInfo", "unitName unitAvatarURL");
 
-  res
-    .status(200)
-    .json({
-      userId,
-      reqId,
-      reportPhoto1Url: donat.report.reportPhoto1Url,
-      reportPhoto2Url: donat.report.reportPhoto2Url,
-      reportPhoto3Url: donat.report.reportPhoto3Url,
-      reportDescr: donat.report.reportDescr,
-      isReported: donat.isReported,
-      isOpen: donat.isOpen,
-    });
+  res.status(200).json({
+    userId,
+    reqId,
+    reportPhoto1Url: donat.report.reportPhoto1Url,
+    reportPhoto2Url: donat.report.reportPhoto2Url,
+    reportPhoto3Url: donat.report.reportPhoto3Url,
+    reportDescr: donat.report.reportDescr,
+    isReported: donat.isReported,
+    isOpen: donat.isOpen,
+    donatDescription: donat.donatDescription,
+    amountOfFee: donat.amountOfFee,
+    typeName: donat.typeName.typeName,
+    owner: donat.owner.name,
+  });
 };
 
 module.exports = addDonatsReport;
